844a5413d2a175692c668233a69ef50a0456b1e5
nagabhushanam <nagabhushanamn@gmail.com>
2016-03-17 07:29:01 +0530

    day-3 commit

---
+++ Shop_v3/js/app.js
@@ -0,0 +1,144 @@
+/**
+ * http://usejsdoc.org/
+ */
+
+var storeModule = angular.module('store', []);
+
+// Controller(s)
+storeModule.controller('StoreController', function($scope, $filter) {
+
+	// logic
+	// this.product = item;
+	// $scope.product = item; // View-Model
+	$scope.products = items;
+
+	console.log($filter('limitTo')($scope.products, 1));
+	console.log($filter('uppercase')('nag'));
+	console.log($filter('priceDiscount')(1000, 100));
+
+});
+
+storeModule.controller('TabsController', function($scope) {
+	$scope.tab = 1; // ng-init="tab=1"
+	$scope.changeTab = function(tabValue) {
+		$scope.tab = tabValue;
+	};
+	$scope.isTabSelected = function(tabValue) {
+		return $scope.tab === tabValue;
+	};
+});
+
+
+// Filter(s)
+storeModule.filter('priceDiscount', function() {
+	return function(originalPrice, discount) {
+		if (discount) {
+			return originalPrice - discount;
+		}
+		return originalPrice - 1;
+	};
+});
+
+
+// Directive(s)
+storeModule.directive("productTitle", function(){
+	return{
+		restrict: "E",
+		replace: true,
+		scope: false,
+		templateUrl: "templates/product-title.html",
+		link:function(scope,tEle,tAttr){
+			tEle.on('mouseenter',function(){
+				tEle.css('background-color','#def');
+			});
+			tEle.on('mouseleave',function(){
+				tEle.css('background-color','#fff');
+			});
+		}
+	};
+});
+storeModule.directive("productTabs", function(){
+	return{
+		restrict: "E",
+		replace: true,
+		scope: false,
+		templateUrl: "templates/product-tabs.html",
+		controller: "TabsController"
+	};
+});
+storeModule.directive("productReviewForm", function(){
+	return{
+		restrict: "E",
+		replace: true,
+		scope: false,
+		templateUrl: "templates/product-review-form.html",
+		controller: function($scope) {
+			$scope.newReview = {
+					user:'nag@gmail.com'
+			};
+			$scope.addNewReview = function(product) {
+				// .....
+				product.reviews.push($scope.newReview);
+				$scope.newReview = {
+						user:'nag@gmail.com'
+				};
+			};
+		}
+	};
+});
+// ------------------------------------------------------------------
+
+// data ( Model )
+var items = [ {
+	name : 'Laptop',
+	price : 148000, // number
+	description : 'New Model',
+	canBuy : true,
+	notAvailable : false,
+	make : Date.now(),
+	reviews : [ {
+		rating : 4,
+		date : Date.now(),
+		comments : 'good to buy',
+		user : 'naga@gmail.com'
+	}, {
+		rating : 3,
+		date : Date.now(),
+		comments : 'bad to buy',
+		user : 'indu@gmail.com'
+	} ]
+}, {
+	name : 'Mobile',
+	price : 48000,
+	description : 'New Model',
+	canBuy : true,
+	notAvailable : false,
+	make : Date.now(),
+	reviews : [ {
+		rating : 4,
+		date : Date.now(),
+		comments : 'good to buy',
+		user : 'naga@gmail.com'
+	}, {
+		rating : 3,
+		date : Date.now(),
+		comments : 'bad to buy',
+		user : 'indu@gmail.com'
+	} ]
+} ];
+
+// ( Imperative Programming )
+
+// using DOM
+
+// plain-JS
+// document.querySelector("h4").innerHTML = item.name;
+// document.querySelector("h5").innerHTML = item.price;
+// document.querySelector("p").innerHTML = item.description;
+
+// jquery
+// $(function(){
+// $("h4").text(item.name);
+// $("h5").text(item.price);
+// $("p").text(item.description);
+// });
